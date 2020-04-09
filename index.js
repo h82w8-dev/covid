a82069226dd1a7057a45e1c576279b5897c78a94
h82w8-dev <56717469+h82w8-dev@users.noreply.github.com>
2020-04-10 01:21:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const Telegraf = require('telegraf');
 const TOKEN = process.env.TOKEN
 const bot = new Telegraf(TOKEN);
 
+
+// commands
+// start, help
 bot.start(ctx => ctx.reply(`
 Welcome to COVID BOT!
 You need to send a name of country where you need to get COVID data
@@ -16,13 +19,13 @@ Russia
 China
 `
 ));
-
+// handlers
 bot.hears(/.*/, async ctx => {
-    const {data} = await covidService.getByCountry(ctx.message.text);
-    
-    if(data && data.results === 0){
-      return ctx.reply('Country not found. Try another');
-    }
+  const {data} = await covidService.getByCountry(ctx.message.text);
   
-    return ctx.replyWithMarkdown(formatCountryMsg(data.response[0]))
-  });
+  if(data && data.results === 0){
+    return ctx.reply('Country not found. Try another');
+  }
+
+  return ctx.replyWithMarkdown(formatCountryMsg(data.response[0]))
+});
